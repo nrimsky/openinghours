c2f77956331dd226e7211e8eb6f9015b82a9c1d8
nrimsky <ninarimsky@gmail.com>
2023-03-03 00:34:48 +0000

    Generated Data

--- eval.py
+++ eval.py
@@ -11,10 +11,13 @@ def read_data(json_path):
 
 def eval(data):
     ans = {}
-    for d in data:
-        r = get_opening_times_json(d)
-        print(str(r))
-        ans[d] = r 
+    for i, d in enumerate(data):
+        try:
+            r = get_opening_times_json(d)
+            print(i)
+            ans[d] = r 
+        except Exception as e:
+            print(f"An exception occurred: {e}, Input: {d}")
     with open(os.path.join("outputs", f'output-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json'), "w") as outfile:
         json.dump(ans, outfile)
     return ans
@@ -22,10 +25,9 @@ def eval(data):
 
 if __name__ == "__main__":
     data = read_data("Toilet-2023-03-02.json") # len = 560
-    eval(data=data[:10])
-    # eval(data=data[:100])
-    # eval(data=data[100:200])
-    # eval(data=data[200:300])
-    # eval(data=data[300:400])
-    # eval(data=data[400:500])
-    # eval(data=data[500:])
+    eval(data=data[:100])
+    eval(data=data[100:200])
+    eval(data=data[200:300])
+    eval(data=data[300:400])
+    eval(data=data[400:500])
+    eval(data=data[500:])
