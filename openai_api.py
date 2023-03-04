15e356c9b6109c64a45c678bcaefda6f2cfdbcf0
nrimsky <ninarimsky@gmail.com>
2023-03-04 18:17:10 +0000

    Update prompt

--- openai_api.py
+++ openai_api.py
@@ -1,7 +1,7 @@
 import json
 import openai
 import certifi
-from prompt import start_prompt
+from prompt import MESSAGES
 
 certifi.where()
 
@@ -14,29 +14,28 @@ openai.api_key = key
 def get_gpt_response(full_prompt, model="gpt-3.5-turbo"):
     completions = openai.ChatCompletion.create(
         model=model,
-        messages =[{"role": "user", "content": full_prompt}],
+        messages=full_prompt,
         max_tokens=1024,
         n=1,
         temperature=0,
     )
 
-    message = completions["choices"][0]["message"]["content"]
+    message = completions["choices"][-1]["message"]["content"]
     return message.strip()
 
 
 def get_opening_times_json(input_sentence):
-
-    full_prompt = start_prompt + input_sentence
-
+    full_prompt = [m for m in MESSAGES]
+    full_prompt.append({"role": "user", "content": input_sentence})
     text = get_gpt_response(full_prompt)
 
     try:
         return json.loads(text)
     except Exception as e:
         if "INSUFFICIENT" not in text.upper():
-            print('Failed to parse response', text, e)
+            print("Failed to parse response", text, e)
         return {}
 
 
 if __name__ == "__main__":
-    print(get_opening_times_json("Mon Sat 1-3pm Sun closed"))
+    print(get_opening_times_json("Mon Wed 9am-8pm Thur Sun 24hrs"))
--- prompt.py
+++ prompt.py
@@ -1,14 +1,22 @@
-start_prompt = """
+SYSTEM_INSTRUCTIONS = """
+You are a parser assistant that turns unstructured natural language opening hours text into a structured valid JSON format with keys of days of the week, and values of hours intervals, using an HH:MM format. 
+If there are different opening times for different times of year, you use the latest opening and earliest closing times found.
+If the description is vague, you use realistic looking opening hours given the information.
+If there is no information given that could be used to parse opening hours, you return "INSUFFICIENT INFORMATION".
+IMPORTANT: If the text looks like it may be missing a dash, you assume a dash is present. For example, 'mon sun' probably means Monday TO Sunday.
+DO NOT provide any additional text, ONLY RETURN VALID JSON!
+ALWAYS use the HH:MM format - terms such as dawn and dusk should always be replaced with realistic HH:MM times.
+"""
 
-We want to be an API that turns unstructured natural language opening hours text into a structured valid JSON format with keys of days of the weeks, values of hours intervals using an HH:MM format. 
-If there are different opening times for different times of year, use the latest opening and earliest closing times found.
-If the description is vague, use realistic looking opening hours given the information.
-If there is no information given that could be used to parse opening hours, return "INSUFFICIENT INFORMATION".
-IMPORTANT: "Mon Sat 1-3pm Sun closed" (missing a dash) should be interpreted as "Monday TO Saturday 1 to 3pm, Sunday closed"
-
-Here are some examples: 
-
-"Open weekdays noon to 4 pm, and 8 pm to midnight, and closed on weekends" should return
+MESSAGES = [
+    {"role": "system", "content": SYSTEM_INSTRUCTIONS},
+    {
+        "role": "user",
+        "content": "Open weekdays noon to 4 pm, and 8 pm to midnight, and closed on weekends",
+    },
+    {
+        "role": "assistant",
+        "content": """
 {
     "Monday": [["12:00", "16:00"], ["20:00", "24:00"]],
     "Tuesday": [["12:00", "16:00"], ["20:00", "24:00"]],
@@ -18,8 +26,15 @@ Here are some examples:
     "Saturday": [],
     "Sunday": []
 }
-
-"The store is open from 9am to 5pm on weekdays and from 10am to 2pm on weekends." should return
+        """,
+    },
+    {
+        "role": "user",
+        "content": "The store is open from 9am to 5pm on weekdays and from 10am to 2pm on weekends.",
+    },
+    {
+        "role": "assistant",
+        "content": """
 {
     "Monday": [["09:00", "17:00"]],
     "Tuesday": [["09:00", "17:00"]],
@@ -29,19 +44,32 @@ Here are some examples:
     "Saturday": [["10:00", "14:00"]],
     "Sunday": [["10:00", "14:00"]]
 }
-
-"Open on wed 11-2 and fri sun 9-6:30" should return
+        """,
+    },
+    {"role": "user", "content": "Open same time as museum"},
+    {
+        "role": "assistant",
+        "content": "INSUFFICIENT INFORMATION",
+    },
+    {"role": "user", "content": "Open 9-11am and 2-7pm expect tuesdays when it is only open from 3pm"},
+    {
+        "role": "assistant",
+        "content": """
 {
-    "Monday": [],
-    "Tuesday": [],
-    "Wednesday": [["11:00", "14:00"]],
-    "Thursday": [],
-    "Friday": [["09:00", "18:30"]],
-    "Saturday": [["09:00", "18:30"]],
-    "Sunday": [["09:00", "18:00"]]
+    "Monday": [["09:00", "11:00"], ["14:00", "19:00"]],
+    "Tuesday": [["15:00", "19:00"]],
+    "Wednesday": [["09:00", "11:00"], ["14:00", "19:00"]],
+    "Thursday": [["09:00", "11:00"], ["14:00", "19:00"]],
+    "Friday": [["09:00", "11:00"], ["14:00", "19:00"]],
+    "Saturday":[["09:00", "11:00"], ["14:00", "19:00"]],
+    "Sunday": [["09:00", "11:00"], ["14:00", "19:00"]]
 }
-
-"May-October: 10:00-18:00, November-April: 9.00-17:00" should return
+        """,
+    },
+    {"role": "user", "content": "May-October: 10:00-18:00, November-April: 9.00-17:00"},
+    {
+        "role": "assistant",
+        "content": """
 {
     "Monday": [["10:00", "17:00"]],
     "Tuesday": [["10:00", "17:00"]],
@@ -51,43 +79,57 @@ Here are some examples:
     "Saturday": [["10:00", "17:00"]],
     "Sunday": [["10:00", "17:00"]]
 }
-
-"Open 9-11am and 2-7pm" should return
+        """,
+    },
+    {"role": "user", "content": "Open on wed 11-2 and fri sun 9-6:30"},
+    {
+        "role": "assistant",
+        "content": """
 {
-    "Monday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Tuesday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Wednesday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Thursday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Friday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Saturday":[["09:00", "11:00"], ["14:00", "19:00"]],
-    "Sunday": [["09:00", "11:00"], ["14:00", "19:00"]]
+    "Monday": [],
+    "Tuesday": [],
+    "Wednesday": [["11:00", "14:00"]],
+    "Thursday": [],
+    "Friday": [["09:00", "18:30"]],
+    "Saturday": [["09:00", "18:30"]],
+    "Sunday": [["09:00", "18:00"]]
 }
-
-"Open same times as museum" should return
-INSUFFICIENT INFORMATION
-
-"Mon Wed Every day from 8:15am 10am then 4pm 9pm, Thur - Sun closed" should return
+        """,
+    },
+    {
+        "role": "user",
+        "content": "Monday - friday 9:30am - Dusk",
+    },
+    {
+        "role": "assistant",
+        "content": """
 {
-    "Monday": [["08:15", "10:00"], ["16:00", "21:00"]], 
-    "Tuesday":  [["08:15", "10:00"], ["16:00", "21:00"]], 
-    "Wednesday":  [["08:15", "10:00"], ["16:00", "21:00"]], 
-    "Thursday": [], 
-    "Friday": [], 
+    "Monday": [["09:30", "17:00"]], 
+    "Tuesday":  [["09:30", "17:00"]], 
+    "Wednesday":  [["09:30", "17:00"]], 
+    "Thursday": [["09:30", "17:00"]], 
+    "Friday": [["09:30", "17:00"]],  
     "Saturday": [], 
     "Sunday": []
 }
-
-"May-Sept mon sat 1-8pm sun closed; Other times mon sat 1-6:30pm except sun" should return
+        """,
+    },
+    {
+        "role": "user",
+        "content": "Mon Sat 1-3pm Sun closed",
+    },
+    {
+        "role": "assistant",
+        "content": """
 {
-    "Monday": [["13:00", "18:30"]], 
-    "Tuesday":  [["13:00", "18:30"]], 
-    "Wednesday":  [["13:00", "18:30"]], 
-    "Thursday": [["13:00", "18:30"]], 
-    "Friday": [["13:00", "18:30"]], 
-    "Saturday": [["13:00", "18:30"]], 
+    "Monday": [["13:00", "15:00"]], 
+    "Tuesday":  [["13:00", "15:00"]], 
+    "Wednesday":  [["13:00", "15:00"]], 
+    "Thursday": [["13:00", "15:00"]], 
+    "Friday": [["13:00", "15:00"]], 
+    "Saturday": [["13:00", "15:00"]], 
     "Sunday": []
 }
-
-Now convert the input below into the valid JSON format:
-
-"""
+        """,
+    }
+]
