4adfee7b33756dabc8065f1099919873347cbab9
nrimsky <ninarimsky@gmail.com>
2023-03-05 20:12:34 +0000

    Use code-davinci-002

--- openai_api.py
+++ openai_api.py
@@ -11,32 +11,33 @@ with open("key.txt", "r") as keyfile:
 openai.api_key = key
 
 
-def get_gpt_response(full_prompt, model="gpt-3.5-turbo"):
-    completions = openai.ChatCompletion.create(
-        model=model,
-        messages =[{"role": "user", "content": full_prompt}],
+def get_gpt_response(full_prompt, model="code-davinci-002"):
+    completions = openai.Completion.create(
+        engine=model,
+        prompt=full_prompt,
         max_tokens=1024,
         n=1,
         temperature=0,
+        stop="}",
     )
-
-    message = completions["choices"][0]["message"]["content"]
+    message = completions["choices"][0]["text"]
+    message = message.strip()
+    if message[-1] != "}":
+        message += "}"
     return message.strip()
 
 
 def get_opening_times_json(input_sentence):
-
-    full_prompt = start_prompt + input_sentence
-
+    full_prompt = start_prompt + f"INPUT: {input_sentence}\nOUTPUT:\n"
     text = get_gpt_response(full_prompt)
 
     try:
         return json.loads(text)
     except Exception as e:
         if "INSUFFICIENT" not in text.upper():
-            print('Failed to parse response', text, e)
+            print("Failed to parse response", text, e)
         return {}
 
 
 if __name__ == "__main__":
-    print(get_opening_times_json("Mon Sat 1-3pm Sun closed"))
+    print(get_opening_times_json("Mon Fri dawn to dusk"))
--- prompt.py
+++ prompt.py
@@ -1,14 +1,14 @@
 start_prompt = """
 
-We want to be an API that turns unstructured natural language opening hours text into a structured valid JSON format with keys of days of the weeks, values of hours intervals using an HH:MM format. 
-If there are different opening times for different times of year, use the latest opening and earliest closing times found.
-If the description is vague, use realistic looking opening hours given the information.
-If there is no information given that could be used to parse opening hours, return "INSUFFICIENT INFORMATION".
+This is an API that turns unstructured natural language opening hours text into a structured valid JSON format with keys of days of the weeks, values of hours intervals using an HH:MM format. 
+If there are different opening times for different times of year, we use the latest opening and earliest closing times found.
+If the description is vague, e.g. using terms such as dawn and dusk, we use the most realistic looking HH:MM opening hours given the information.
+If there is no information given that could be used to parse opening hours, we return "INSUFFICIENT INFORMATION".
 IMPORTANT: "Mon Sat 1-3pm Sun closed" (missing a dash) should be interpreted as "Monday TO Saturday 1 to 3pm, Sunday closed"
 
-Here are some examples: 
-
-"Open weekdays noon to 4 pm, and 8 pm to midnight, and closed on weekends" should return
+Examples of input/output pairs: 
+INPUT: "Open weekdays noon to 4 pm, and 8 pm to midnight, and closed on weekends"
+OUTPUT:
 {
     "Monday": [["12:00", "16:00"], ["20:00", "24:00"]],
     "Tuesday": [["12:00", "16:00"], ["20:00", "24:00"]],
@@ -18,19 +18,8 @@ Here are some examples:
     "Saturday": [],
     "Sunday": []
 }
-
-"The store is open from 9am to 5pm on weekdays and from 10am to 2pm on weekends." should return
-{
-    "Monday": [["09:00", "17:00"]],
-    "Tuesday": [["09:00", "17:00"]],
-    "Wednesday": [["09:00", "17:00"]],
-    "Thursday": [["09:00", "17:00"]],
-    "Friday": [["09:00", "17:00"]],
-    "Saturday": [["10:00", "14:00"]],
-    "Sunday": [["10:00", "14:00"]]
-}
-
-"Open on wed 11-2 and fri sun 9-6:30" should return
+INPUT: "Open on wed 11-2 and fri sun 9-6:30"
+OUTPUT:
 {
     "Monday": [],
     "Tuesday": [],
@@ -40,8 +29,8 @@ Here are some examples:
     "Saturday": [["09:00", "18:30"]],
     "Sunday": [["09:00", "18:00"]]
 }
-
-"May-October: 10:00-18:00, November-April: 9.00-17:00" should return
+INPUT: "May-October: 10:00-18:00, November-April: 9.00-17:00"
+OUTPUT:
 {
     "Monday": [["10:00", "17:00"]],
     "Tuesday": [["10:00", "17:00"]],
@@ -51,22 +40,22 @@ Here are some examples:
     "Saturday": [["10:00", "17:00"]],
     "Sunday": [["10:00", "17:00"]]
 }
-
-"Open 9-11am and 2-7pm" should return
+INPUT: "Open 9-11am and 2-dusk"
+OUTPUT:
 {
-    "Monday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Tuesday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Wednesday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Thursday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Friday": [["09:00", "11:00"], ["14:00", "19:00"]],
-    "Saturday":[["09:00", "11:00"], ["14:00", "19:00"]],
-    "Sunday": [["09:00", "11:00"], ["14:00", "19:00"]]
+    "Monday": [["09:00", "11:00"], ["14:00", "18:00"]],
+    "Tuesday": [["09:00", "11:00"], ["14:00", "18:00"]],
+    "Wednesday": [["09:00", "11:00"], ["14:00", "18:00"]],
+    "Thursday": [["09:00", "11:00"], ["14:00", "18:00"]],
+    "Friday": [["09:00", "11:00"], ["14:00", "18:00"]],
+    "Saturday":[["09:00", "11:00"], ["14:00", "18:00"]],
+    "Sunday": [["09:00", "11:00"], ["14:00", "18:00"]]
 }
-
-"Open same times as museum" should return
+INPUT: "Open same times as museum"
+OUTPUT:
 INSUFFICIENT INFORMATION
-
-"Mon Wed Every day from 8:15am 10am then 4pm 9pm, Thur - Sun closed" should return
+INPUT: "Mon Wed Every day from 8:15am 10am then 4pm 9pm, Thur - Sun closed"
+OUTPUT:
 {
     "Monday": [["08:15", "10:00"], ["16:00", "21:00"]], 
     "Tuesday":  [["08:15", "10:00"], ["16:00", "21:00"]], 
@@ -76,8 +65,8 @@ INSUFFICIENT INFORMATION
     "Saturday": [], 
     "Sunday": []
 }
-
-"May-Sept mon sat 1-8pm sun closed; Other times mon sat 1-6:30pm except sun" should return
+INPUT: "May-Sept mon sat 1-8pm sun closed; Other times mon sat 1-6:30pm except sun"
+OUTPUT:
 {
     "Monday": [["13:00", "18:30"]], 
     "Tuesday":  [["13:00", "18:30"]], 
@@ -87,7 +76,4 @@ INSUFFICIENT INFORMATION
     "Saturday": [["13:00", "18:30"]], 
     "Sunday": []
 }
-
-Now convert the input below into the valid JSON format:
-
 """
