d204d15055e743d7d9cc4ff2679c24dbe4554314
Nina <ninarimsky@gmail.com>
2023-03-05 21:34:45 +0000

    Print exception

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 def my_api():
     data = request.get_json()
     opening_hours = data["opening_hours"]
-    parsed = get_opening_times_json_codex(opening_hours)
+    try:
+        parsed = get_opening_times_json_codex(opening_hours)
+    except Exception as e:
+        print(e)
+        return {}
     return parsed
 
 if __name__ == '__main__':
